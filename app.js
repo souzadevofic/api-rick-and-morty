38fc5f0882906351269373f468fc334ab50f795f
Alisson Souza <alissonso432@gmail.com>
2024-07-15 00:03:48 -0300

    card estilizado com sucesso

--- app.js
+++ app.js
@@ -5,26 +5,28 @@ fetch('https://rickandmortyapi.com/api/character')
     }
     return response.json();
 })
-.then(data => {
-    
+.then(data => {    
     const div = document.querySelector('.chars-container');
 
     data.results.forEach(personagem => {
         const personagemDiv = document.createElement('div');
         personagemDiv.classList.add('personagem');
-
-        personagemDiv.innerHTML = `Nome: ${personagem.name} | Tipo: ${personagem.species}`
         
-
         const foto = document.createElement('img');
         foto.setAttribute('src', personagem.image);
         foto.classList.add('foto');
-
+    
         personagemDiv.appendChild(foto);
         div.appendChild(personagemDiv);
+        
+        personagemDiv.innerHTML += `
+        <section class="card-persons">
+        <div>Nome: ${personagem.name}</div>
+        <div>Tipo: ${personagem.species}</div> 
+        <div>Status: ${personagem.status}</div>
+        </section>`
+       
     })
 
-    function buscarPersonagem(){
-
-    }
 });
+
